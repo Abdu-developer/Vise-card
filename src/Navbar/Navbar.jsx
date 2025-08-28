e21b63a24e1a02d73a390c25636d32374a63342a
Abdu-developer <abdumannopsaydullayev8@gmail.com>
2025-08-27 22:58:22 -0700

    .

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 import './Navbar.css'
 const Navbar = () => {
   return (
-    <div >Navbar</div>
+    <div className='navbar'>
+        <h1>Quo.</h1>
+        <ul>
+          <li><a href="">Money Transfer</a></li>
+          <li><a href="">Multi Currency</a></li>
+          <li><a href="">How it works</a></li>
+          <li><a href="">Business</a></li>
+          <li><a href="">Help</a></li>
+          <li><a href="">Log In</a></li>
+
+        </ul>
+    </div>
   )
 }
 
